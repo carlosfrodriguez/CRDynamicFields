0d1cf39650b21ca021355e29e4d7258336e96ed2
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2014-02-11 16:51:49 -0600

    Initial version of Radio Button Dynamic Field.

---
+++ Kernel/System/DynamicField/Driver/CRRadioButton.pm
@@ -0,0 +1,278 @@
+# --
+# Kernel/System/DynamicField/Driver/CRRadioButton.pm - Delegate for DynamicField RadioButton backend
+# Copyright (C) 2001-2014 Carlos Rodriguez, https://github.com/carlosfrodriguez
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+# Most of the code in this file is property of OTRS AG!
+# This file was inspired in otrs Dropdown.pm,v 1.47
+#
+# otrs is Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
+# --
+
+package Kernel::System::DynamicField::Driver::CRRadioButton;
+
+use strict;
+use warnings;
+
+use Kernel::System::VariableCheck qw(:all);
+use Kernel::System::DynamicFieldValue;
+
+use base qw(Kernel::System::DynamicField::Driver::BaseSelect);
+
+=head1 NAME
+
+Kernel::System::DynamicField::Driver::RadioButton
+
+=head1 SYNOPSIS
+
+DynamicFields RadioButton driver delegate
+
+=head1 PUBLIC INTERFACE
+
+This module implements the public interface of L<Kernel::System::DynamicField::Backend>.
+Please look there for a detailed reference of the functions.
+
+=over 4
+
+=item new()
+
+usually, you want to create an instance of this
+by using Kernel::System::DynamicField::Backend->new();
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # get needed objects
+    for my $Needed (qw(ConfigObject EncodeObject LogObject MainObject DBObject)) {
+        die "Got no $Needed!" if !$Param{$Needed};
+
+        $Self->{$Needed} = $Param{$Needed};
+    }
+
+    # create additional objects
+    $Self->{DynamicFieldValueObject} = Kernel::System::DynamicFieldValue->new( %{$Self} );
+
+    # set field behaviors
+    $Self->{Behaviors} = {
+        'IsACLReducible'               => 1,
+        'IsNotificationEventCondition' => 1,
+        'IsSortable'                   => 0,
+        'IsFiltrable'                  => 0,
+        'IsStatsCondition'             => 1,
+        'IsCustomerInterfaceCapable'   => 1,
+    };
+
+    # get the Dynamic Field Backend custmom extensions
+    my $DynamicFieldDriverExtensions
+        = $Self->{ConfigObject}->Get('DynamicFields::Extension::Driver::Multiselect');
+
+    EXTENSION:
+    for my $ExtensionKey ( sort keys %{$DynamicFieldDriverExtensions} ) {
+
+        # skip invalid extensions
+        next EXTENSION if !IsHashRefWithData( $DynamicFieldDriverExtensions->{$ExtensionKey} );
+
+        # create a extension config shortcut
+        my $Extension = $DynamicFieldDriverExtensions->{$ExtensionKey};
+
+        # check if extension has a new module
+        if ( $Extension->{Module} ) {
+
+            # check if module can be loaded
+            if ( !$Self->{MainObject}->RequireBaseClass( $Extension->{Module} ) ) {
+                die "Can't load dynamic fields backend module"
+                    . " $Extension->{Module}! $@";
+            }
+        }
+
+        # check if extension contains more behabiors
+        if ( IsHashRefWithData( $Extension->{Behaviors} ) ) {
+
+            %{ $Self->{Behaviors} } = (
+                %{ $Self->{Behaviors} },
+                %{ $Extension->{Behaviors} }
+            );
+        }
+    }
+
+    return $Self;
+}
+
+sub EditFieldRender {
+    my ( $Self, %Param ) = @_;
+
+    # take config from field config
+    my $FieldConfig = $Param{DynamicFieldConfig}->{Config};
+    my $FieldName   = 'DynamicField_' . $Param{DynamicFieldConfig}->{Name};
+    my $FieldLabel  = $Param{DynamicFieldConfig}->{Label};
+
+    my $Value;
+
+    # set the field value or default
+    if ( $Param{UseDefaultValue} ) {
+        $Value = ( defined $FieldConfig->{DefaultValue} ? $FieldConfig->{DefaultValue} : '' );
+    }
+    $Value = $Param{Value} // $Value;
+
+    # check if a value in a template (GenericAgent etc.)
+    # is configured for this dynamic field
+    if (
+        IsHashRefWithData( $Param{Template} )
+        && defined $Param{Template}->{$FieldName}
+        )
+    {
+        $Value = $Param{Template}->{$FieldName};
+    }
+
+    # extract the dynamic field value form the web request
+    my $FieldValue = $Self->EditFieldValueGet(
+        %Param,
+    );
+
+    # set values from ParamObject if present
+    if ( defined $FieldValue ) {
+        $Value = $FieldValue;
+    }
+
+    # check and set class if necessary
+    my $FieldClass = 'DynamicFieldText';
+    if ( defined $Param{Class} && $Param{Class} ne '' ) {
+        $FieldClass .= ' ' . $Param{Class};
+    }
+
+    # set field as mandatory
+    if ( $Param{Mandatory} ) {
+        $FieldClass .= ' Validate_Required';
+    }
+
+    # set error css class
+    if ( $Param{ServerError} ) {
+        $FieldClass .= ' ServerError';
+    }
+
+    # set PossibleValues, use PossibleValuesFilter if defined
+    my $PossibleValues = $Param{PossibleValuesFilter} // $Self->PossibleValuesGet(%Param);
+
+    my $Colunms = $FieldConfig->{Columns} || 3;
+    my $ColumnItemCounter;
+
+    my $HTMLString;
+
+    ITEMID:
+    for my $ItemID ( sort keys %{$PossibleValues} ) {
+        $ColumnItemCounter ++;
+
+        my $Selected = '';
+        if ( $ItemID eq $Value ){
+            $Selected = 'selected="Selected"';
+        }
+
+        $HTMLString .= <<"EOF";
+<input type="Radio" name="$FieldName" class="$FieldClass" value="$ItemID" $Selected/>$PossibleValues->{$ItemID}
+EOF
+
+        # place radio boxes as a row intil a column change
+        if ( $ColumnItemCounter == $Colunms ) {
+            $HTMLString .= <<"EOF";
+<div class="Clear"></div>
+EOF
+
+        # reset column counter for the next row
+        $ColumnItemCounter = 0;
+        }
+    }
+
+    if ( $Param{Mandatory} ) {
+        my $DivID = $FieldName . 'Error';
+
+        # for client side validation
+        $HTMLString .= <<"EOF";
+
+<div id="$DivID" class="TooltipErrorMessage">
+    <p>
+        \$Text{"This field is required."}
+    </p>
+</div>
+EOF
+    }
+
+    if ( $Param{ServerError} ) {
+
+        my $ErrorMessage = $Param{ErrorMessage} || 'This field is required.';
+        my $DivID = $FieldName . 'ServerError';
+
+        # for server side validation
+        $HTMLString .= <<"EOF";
+
+<div id="$DivID" class="TooltipErrorMessage">
+    <p>
+        \$Text{"$ErrorMessage"}
+    </p>
+</div>
+EOF
+    }
+
+    if ( $Param{AJAXUpdate} ) {
+
+        my $FieldSelector = '#' . $FieldName;
+
+        my $FieldsToUpdate = '';
+        if ( IsArrayRefWithData( $Param{UpdatableFields} ) ) {
+
+            # Remove current field from updatable fields list
+            my @FieldsToUpdate = grep { $_ ne $FieldName } @{ $Param{UpdatableFields} };
+
+            # quote all fields, put commas in between them
+            $FieldsToUpdate = join( ', ', map {"'$_'"} @FieldsToUpdate );
+        }
+
+        # add js to call FormUpdate()
+        $HTMLString .= <<"EOF";
+
+<!--dtl:js_on_document_complete-->
+<script type="text/javascript">//<![CDATA[
+    \$('$FieldSelector').bind('change', function (Event) {
+        Core.AJAX.FormUpdate(\$(this).parents('form'), 'AJAXUpdate', '$FieldName', [ $FieldsToUpdate ]);
+    });
+//]]></script>
+<!--dtl:js_on_document_complete-->
+EOF
+    }
+
+    # call EditLabelRender on the common Driver
+    my $LabelString = $Self->EditLabelRender(
+        DynamicFieldConfig => $Param{DynamicFieldConfig},
+        Mandatory          => $Param{Mandatory} || '0',
+        FieldName          => $FieldName,
+    );
+
+    my $Data = {
+        Field => $HTMLString,
+        Label => $LabelString,
+    };
+
+    return $Data;
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (L<http://otrs.org/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
